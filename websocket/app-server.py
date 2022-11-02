c0bc7e280916a252e8c1de41a393d85f7d5087b7
Hassan <hassan8ghannoum@gmail.com>
2022-11-02 17:36:17 +0200

    send the message

--- websocket/app-server.py
+++ websocket/app-server.py
@@ -4,11 +4,9 @@ import websockets
 async def echo(websocket):
     while 1:
         async for message in websocket:
-            # process the message
-            print(message)
             if message == 'exit':
                 break
-            await websocket.send(message)
+            await websocket.send('message')
 
 async def main():
     async with websockets.serve(echo, "0.0.0.0", 12001, max_size=1948576):
